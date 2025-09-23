b6605a707dd0e49a7a6b9df3ab8ab65436b4aec4
Juan Holy <59853386+JuanHDSM@users.noreply.github.com>
2025-09-23 17:40:36 -0400

    feat: centralizing panel, fix: params logic (#1)
    * feat: centralizing panel, fix: params logic
    
    * feat: added Manifest Info
    
    * feat: README.md
    
    * Update README.md
    
    * Update README.md
    
    * fix compile step
    
    * Update README.md
    
    ---------
    
    Co-authored-by: Juan Holy <juan.holy@gertec.com.br>

--- README.md
+++ README.md
@@ -1,1 +1,46 @@
-# generate-password-offline
+# Generate Password Offline Application
+
+## Requirements
+
+- Java installed
+  
+
+## Clone repository
+
+- run follow command on terminal:
+```
+git clone https://github.com/JuanHDSM/generate-password-offline.git
+```
+
+- go to directory:
+```
+cd generate-password-offline/src
+```
+
+## Compile Class
+
+- run the follow command
+
+```
+javac Main.java
+```
+
+## Create Manifest
+```
+ echo "Main-Class: Main" > Manifest.txt
+ echo "" >> Manifest.txt
+```
+
+## Generate jar file
+
+-  run the follow command:
+
+```
+jar cfm Main.jar Manifest.txt Main.class
+```
+
+## Run jar file
+
+```
+java -jar Main.jar <size-of-passoword-or-empty>
+```
--- src/Main.java
+++ src/Main.java
@@ -1,61 +1,75 @@
-import java.awt.FlowLayout;
+import java.awt.Component;
+import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
 import java.awt.Toolkit;
 import java.awt.datatransfer.Clipboard;
 import java.awt.datatransfer.StringSelection;
 import java.security.SecureRandom;
 import javax.swing.Box;
+import javax.swing.BoxLayout;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
-import javax.swing.JTextArea;
 
 public class Main {
     public static void main(String[] args) {
 
+        // Frames
         JFrame miJFrame = new JFrame("Exemplo - Java Swing");
         miJFrame.setSize(500, 300);
-        JPanel miJPanel = new JPanel();
-        JPanel miJPanel2 = new JPanel();
-        miJPanel.setSize(300, 400);
-        miJPanel2.setSize(300, 20);
+        miJFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-        miJPanel2.setLayout(new GridBagLayout());
-        miJPanel.setLayout(new FlowLayout());
+        // Panels
+        JPanel mainPanel = new JPanel();
+        JPanel passPanel = new JPanel();
+        passPanel.setSize(300, 200);
 
-        JLabel miJLabel = new JLabel();
-        JLabel miJLabel2 = new JLabel();
+        // Labels
+        JLabel textCopiedLabel = new JLabel();
+        JLabel passLabel = new JLabel();
 
-        JTextArea miJTextArea = new JTextArea(1, 5);
+        // Layout
+        BoxLayout boxLayout = new BoxLayout(passPanel, BoxLayout.Y_AXIS);
+        GridBagLayout gridBagLayout = new GridBagLayout();
 
-        JButton bt = new JButton("Gerar");
+        // Buttons
+        JButton bt = new JButton("Gerar Senha");
 
-        //        miJPanel.add(miJTextArea);
-        miJPanel2.add(bt);
-        miJPanel2.add(Box.createHorizontalStrut(10));
-        miJPanel.add(miJLabel);
-        miJPanel2.add(miJLabel2);
+        mainPanel.setLayout(gridBagLayout);
+        passPanel.setLayout(boxLayout);
+        passPanel.add(marginTopBottom());
+        passPanel.add(passLabel);
+        passPanel.add(marginTopBottom());
+        passPanel.add(bt);
+        passPanel.add(marginTopBottom());
+        passPanel.add(textCopiedLabel);
+
+        GridBagConstraints gbc = new GridBagConstraints();
+        gbc.gridx = 0;
+        gbc.gridy = 0;
+        gbc.anchor = GridBagConstraints.CENTER; // Center the component
+        mainPanel.add(passPanel, gbc);
 
         bt.addActionListener(e -> {
             String senha = generatePassword(args);
-            miJLabel.setText("Senha: " + senha);
-            miJLabel2.setText("Senha cópiada");
-            textCopy(senha);
+            passLabel.setText("Senha: " + senha);
+            textCopiedLabel.setText("\nSenha cópiada");
+            copyPass(senha);
 
-            miJPanel.add(miJLabel);
-            miJFrame.add(miJPanel);
         });
-        miJLabel.setText("Senha: ");
-        miJLabel2.setText("Sen222ha: ");
-
-        miJFrame.add(miJPanel);
-        miJFrame.add(miJPanel2);
+        
+        miJFrame.add(mainPanel);
+        miJFrame.setLocationRelativeTo(null);
 
         miJFrame.setVisible(true);
     }
+    
+    static Component marginTopBottom() {
+        return Box.createVerticalStrut(10);
+    }
 
-    static void textCopy(String pass) {
+    static void copyPass(String pass) {
         Clipboard cb = Toolkit.getDefaultToolkit().getSystemClipboard();
 
         StringSelection selection = new StringSelection(pass);
@@ -71,7 +85,7 @@ public class Main {
         byte random8 = (byte) senha.nextInt(8);
         try {
             for (int a = 0; a < Integer.parseInt(args[0]); a++) {
-                digitos((byte) senha.nextInt(87));
+                sb.append(digitos((byte) senha.nextInt(87)));
                 if (a % (7 + random8) == 0) {
                     random8 = (byte) senha.nextInt(8);
                     senha = new SecureRandom();
