e4939db18a5848b6ffee933e864900776fcf84ff
debora.vivia <debora.vivia@gertec.com.br>
2025-09-23 18:58:25 -0400

    checkbox adicionado

--- src/Main.java
+++ src/Main.java
@@ -11,6 +11,8 @@ import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JCheckBox;
+
 
 public class Main {
     public static void main(String[] args) {
@@ -19,7 +21,7 @@ public class Main {
         JFrame miJFrame = new JFrame("Exemplo - Java Swing");
         miJFrame.setSize(500, 300);
         miJFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-
+        
         // Panels
         JPanel mainPanel = new JPanel();
         JPanel passPanel = new JPanel();
@@ -36,6 +38,14 @@ public class Main {
         // Buttons
         JButton bt = new JButton("Gerar Senha");
 
+        // checkBox
+        JCheckBox maiusculosBox = new JCheckBox("LETRAS MAIUSCULAS",true);
+        JCheckBox minusculobox = new JCheckBox("LETRAS MINUSCULAS", true);
+        JCheckBox numerosBox = new JCheckBox("NÚMEROS",false);
+        JCheckBox caracterBox = new JCheckBox("CARACTERES",true);
+
+        
+
         mainPanel.setLayout(gridBagLayout);
         passPanel.setLayout(boxLayout);
         passPanel.add(marginTopBottom());
@@ -44,15 +54,19 @@ public class Main {
         passPanel.add(bt);
         passPanel.add(marginTopBottom());
         passPanel.add(textCopiedLabel);
+        passPanel.add(maiusculosBox);
+        passPanel.add(minusculobox);
+        passPanel.add(numerosBox);
+        passPanel.add(caracterBox);
 
         GridBagConstraints gbc = new GridBagConstraints();
         gbc.gridx = 0;
         gbc.gridy = 0;
         gbc.anchor = GridBagConstraints.CENTER; // Center the component
         mainPanel.add(passPanel, gbc);
-
+    
         bt.addActionListener(e -> {
-            String senha = generatePassword(args);
+            String senha = generatePassword(args, numerosBox.isSelected(), maiusculosBox.isSelected(), maiusculosBox.isSelected(), caracterBox.isSelected());
             passLabel.setText("Senha: " + senha);
             textCopiedLabel.setText("\nSenha cópiada");
             copyPass(senha);
@@ -77,7 +91,7 @@ public class Main {
         cb.setContents(selection, null);
     }
 
-    static String generatePassword(String[] args) {
+    static String generatePassword(String[] args, boolean numeros, boolean minusculo, boolean maiusculo, boolean caracteres ) {
 
         SecureRandom senha = new SecureRandom();
         StringBuilder sb = new StringBuilder();
@@ -85,7 +99,7 @@ public class Main {
         byte random8 = (byte) senha.nextInt(8);
         try {
             for (int a = 0; a < Integer.parseInt(args[0]); a++) {
-                sb.append(digitos((byte) senha.nextInt(87)));
+                sb.append(digitos(((byte) senha.nextInt(87)), numeros, maiusculo, minusculo, caracteres));
                 if (a % (7 + random8) == 0) {
                     random8 = (byte) senha.nextInt(8);
                     senha = new SecureRandom();
@@ -94,27 +108,27 @@ public class Main {
         }
         catch (Exception e) {
             for (byte a = 0; a < 16; a++) {
-                sb.append((digitos((byte) senha.nextInt(87))));
+                sb.append((digitos(((byte) senha.nextInt(87)),numeros, maiusculo, minusculo,caracteres )));
             }
         }
         return sb.toString();
     }
 
-    static String digitos(byte random62) {
+    static String digitos(byte random62, boolean numeros, boolean minusculo, boolean maiusculo, boolean caracteres) {
         String especiais = "!@#$%^&*()-_=+[]{};:,.<>?/";
         StringBuilder sb = new StringBuilder();
 
-        if (random62 < 26) {
+        if (random62 < 26 && minusculo ) {
             sb.append(((char) (random62 + 97)));
         }
-        else if (random62 < 52) {
+        else if (random62 < 52 && maiusculo ) {
             sb.append(((char) (random62 + 39)));
         }
         //imprime número
-        else if (random62 < 62) {
+        else if (random62 < 62 && numeros ) {
             sb.append((random62 - 52));
         }
-        else {
+        else if (caracteres){
             int index = random62 % especiais.length();
             sb.append(especiais.charAt(index));
         }
